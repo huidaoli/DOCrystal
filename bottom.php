aae648805b90e97e938f6ca90df0a8347876b49c
huidaoli <1069945237@qq.com>
2013-05-14 23:11:51 +0800

    DOCrystal
    DOCrystal

---
+++ bottom.php
@@ -0,0 +1,25 @@
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<table width="766" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td height="4" background="images/line.gif"></td>
+  </tr>
+  <tr>
+    <td height="44" bgcolor="#F0F0F0"><table width="100%" height="75" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td height="75" align="center"><table width="500" border="0" cellspacing="0" cellpadding="0">
+	<tr>	
+	<td><div id="footer">
+	<div align="center"><a href="http://can2do.com">中国.南京凯之渡信息技术有限公司网站首页</a></div>
+	</div></td>
+	</tr>
+          <tr>
+            <td height="20" align="center">All CopyRights reserved 2012 中国.南京凯之渡信息技术有限公司</td>
+          </tr>
+          <tr>
+            <td height="23" align="center">客服热线：1334019**** E-mail：1069945237@qq.com</td>
+          </tr>
+        </table></td>
+      </tr>
+    </table>    </td>
+  </tr>
+</table>
